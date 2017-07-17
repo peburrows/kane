42a6efe5447f7dafe5542256cc02a782a8c2a204
Phil Burrows <github@philburrows.com>
2017-07-17 10:22:17 -0400

    update to Elixir ~> 1.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Kane.Mixfile do
   def project do
     [app: :kane,
      version: "0.3.4",
-     elixir: "~> 1.2",
+     elixir: "~> 1.4",
      package: package(),
      description: description(),
      deps: deps()]
